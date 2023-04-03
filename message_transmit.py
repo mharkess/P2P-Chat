c2c00ad7e0e81ac48d4c8d9f36911457e0be1946
mharkess <mharkess@bu.edu>
2023-04-03 13:20:31 -0400

    Added functions to start message server and message handling

--- message_transmit.py
+++ message_transmit.py
@@ -1,1 +1,17 @@
 """Module to transmit messages between users"""
+import socket
+
+def message_connection(user):
+    """Establishes connection to another user"""
+    
+
+def start_message_server():
+    """Creates a socket server to recieve messages from users"""
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("localhost", 3452))
+    return server
+
+
+def send_message(message, user):
+    """Sends a message to another user"""
+    message_connection(user)
