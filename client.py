2b4124e1e3771894ac02e04733a386c812b953b2
mharkess <mharkess@bu.edu>
2023-04-05 13:16:17 -0400

    Added Message sending to client

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 """Client-side portion of the p2p application"""
+import threading
 import db_connector as dbc
 import message_transmit as mt
 import user_discovery as ud
@@ -13,4 +14,16 @@ def main():
     first_time_setup()
     ud.update_discovery()
     while True:
-        print("Comms")
+        if not user_lock:
+            user = input('\nEnter the user you want to chat with: ')
+            s_connection = mt.message_connection(user)
+            user_lock = True
+        message = input('\n >>User: ')
+        if message == 'QChat':  # Will quit the program
+            break
+        if message == 'SChat':  # Will switch user to chat to
+            user_lock = False
+        else:
+            s_connection.send(message.encode('ascii'))
+            msg_recv = s_connection.recv(1024)
+            print('\n >> %s: %s', user, str(msg_recv.decode('ascii')))
